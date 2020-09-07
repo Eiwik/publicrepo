d3a4ecf438ccb9d6e81c2e9120aea976a6b8b73f
Eiwik <voloshyn.ihor.work@gmail.com>
2020-09-07 18:31:04 +0300

    Added DB

--- song_words.py
+++ song_words.py
@@ -6,16 +6,21 @@ from re import findall
 from tkinter import *
 from os import startfile
 from tkinter import messagebox
+import sqlite3
+
 
 def isLink(link):
     if findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', link):
         return True
     return False
+
+
 def remove_tags(text):
     try:
         return ''.join(xml.etree.ElementTree.fromstring(text).itertext()).replace('*', '')
     except SyntaxError:
         messagebox.showerror("Error", "Not a valid link")
+
 
 class Song:
     def __init__(self, link, lang):
@@ -23,6 +28,7 @@ class Song:
         self.lang = lang
         self.name = remove_tags(str(BeautifulSoup(urllib.request.urlopen(self.link), 'lxml').find('h2')))
     
+
     def downloadWords(self):
         words = BeautifulSoup(urllib.request.urlopen(self.link), 'lxml').findAll('div', class_=self.lang)
         for i in range(0, len(words)):
@@ -35,7 +41,36 @@ class Song:
 
     def about(self):
         return f"Song name: {self.name}\nLanguage: {self.lang}\nLink: {self.link}"
-#comments
+
+
+class Database:
+    def __init__(self):
+        self.db = sqlite3.connect('songs.db')
+        self.cursor = self.db.cursor()
+        self.ex = self.cursor.execute
+        self.create = self.ex("""CREATE TABLE IF NOT EXISTS Songs(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            TITLE VARCHAR(255) NOT NULL,
+            LINK VARCHAR(255) NOT NULL,
+            LANGUAGE VARCHAR(255) NOT NULL
+        )""")
+
+
+    def insertSong(self, Song):
+        title = Song.name
+        lang = Song.lang
+        link = Song.link
+        self.ex("""INSERT INTO Songs(TITLE, LINK, LANGUAGE)
+                    VALUES(?,?,?);""", (title, link, lang))
+        self.db.commit()
+
+    def getSongsList(self):
+        songsList = self.ex("""SELECT * FROM Songs""").fetchall()
+        for song in songsList:
+            print(f'\nSong #{song[0]}\nSong Title: {song[1]}\nSong link: {song[2]}\nLanguage: {song[3]}')
+            
+
+
 class Block:
     def __init__(self, master):
         self.frame = LabelFrame()
@@ -51,8 +86,10 @@ class Block:
         for elem in ['original', 'translate']:
             self.lb.insert(0, elem)
 
+
     def setFunc(self, func):
         self.b['command'] = eval(f'self.{func}')
+
 
     def downloadLyrics(self):
         link = self.e.get()
@@ -63,6 +100,10 @@ class Block:
             Song(link, lang).downloadWords()
             self.frame['text'] = Song(link, lang).name
             self.l['text'] = Song(link, lang).about()
+        openDatabase = Database()
+        openDatabase.insertSong(Song(link, lang))
+        openDatabase.getSongsList()
+
 
 root = Tk()
 root.resizable(False, False)
